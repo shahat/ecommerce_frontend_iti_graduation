434fa3a15041d295f88d0a011ce975c8005a3c1c
Edward Sameh <eddysameh@yahoo.com>
2023-10-29 16:15:07 +0200

    review reusble component & toggle inhancement

--- src/pages/ProductDetails/ProductDetails.jsx
+++ src/pages/ProductDetails/ProductDetails.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useEffect } from "react";
 import css from "./product.module.css";
 import PhotoGallery from "../../components/productDetailsComps/photoGallery";
+import ProductReview from "../../components/productDetailsComps/productReview";
 import {
     FaFilter,
     FaStar,
@@ -12,29 +13,34 @@ import {
 } from "react-icons/fa6";
 
 function ProductDetails() {
-    var toggelButtons = document.getElementsByClassName("toggel");
-    var toggelPages = document.querySelectorAll("div[id*='tab-pane-']");
 
-    for (var i = 0; i < toggelButtons.length; i++) {
-        toggelButtons[i].addEventListener("click", switchPages);
-        // Add a custom data attribute to store the index
-        toggelButtons[i].setAttribute("button-index", i);
-    }
-
-    function switchPages(e) {
-        var button = e.target;
-        // Get the index from the data attribute
-        var index = button.getAttribute("button-index");
-
+    useEffect(()=>{
+        var toggelButtons = document.getElementsByClassName("toggel");
+        var toggelPages = document.querySelectorAll("div[id*='tab-pane-']");
+    
         for (var i = 0; i < toggelButtons.length; i++) {
-            toggelButtons[i].classList.remove("border-bottom-0", "border");
-            toggelButtons[i].classList.add("border-bottom");
-            toggelPages[i].classList.add("d-none");
+            toggelButtons[i].addEventListener("click", switchPages);
+            // Add a custom data attribute to store the index
+            toggelButtons[i].setAttribute("button-index", i);
         }
-        button.classList.add("border-bottom-0", "border");
-        button.classList.remove("border-bottom");
-        toggelPages[index].classList.remove("d-none");
-    }
+    
+        function switchPages(e) {
+            var button = e.target;
+            // Get the index from the data attribute
+            var index = button.getAttribute("button-index");
+    
+            for (var i = 0; i < toggelButtons.length; i++) {
+                toggelButtons[i].classList.remove("border-bottom-0", "border");
+                toggelButtons[i].classList.add("border-bottom");
+                toggelPages[i].classList.add("d-none");
+            }
+            button.classList.add("border-bottom-0", "border");
+            button.classList.remove("border-bottom");
+            toggelPages[index].classList.remove("d-none");
+        }
+    }, [])
+
+   
 
     return (
         <>
@@ -312,24 +318,24 @@ function ProductDetails() {
                     className={`${css.details} product row d-flex col-md-10 justify-content-center m-0 p-0`}
                 >
                     <div className="row col-10">
-                        <a
-                            href="#tab-pane-1"
+                        <button
+                            // href="#tab-pane-1"
                             className="toggel btn pb-2 mb-4 text-danger border-bottom rounded-bottom-0 col"
                         >
                             Product Details
-                        </a>
-                        <a
-                            href="#tab-pane-2"
+                        </button>
+                        <button
+                            // href="#tab-pane-2"
                             className="toggel btn pb-2 mb-4 text-danger rounded-bottom-0 border-bottom-0 col border"
                         >
                             Ratings & Reviews
-                        </a>
-                        <a
-                            href="#tab-pane-3"
+                        </button>
+                        <button
+                            // href="#tab-pane-3"
                             className="toggel btn pb-2 mb-4 text-danger border-bottom rounded-bottom-0 col"
                         >
                             FAQs
-                        </a>
+                        </button>
                     </div>
 
                     <div className="container">
@@ -408,56 +414,12 @@ function ProductDetails() {
                                 <div
                                     className={`${css["reviews-individuals"]} d-flex flex-md-row flex-column flex-wrap justify-content-center`}
                                 >
-                                    <div className="single-review border rounded-4 col-lg-5 col-12 p-3">
-                                        <div className="review-h d-flex justify-content-between mb-3">
-                                            <div className="text-secondary d-flex">
-                                                <FaStar className="text-warning" />
-                                                <FaStar className="text-warning" />
-                                                <FaStar className="text-warning" />
-                                                <FaStar className="text-warning" />
-                                                <FaStar />
-                                            </div>
-                                            <div className="review-option">
-                                                <button className="btn">
-                                                    <i className="fa-solid fa-ellipsis"></i>
-                                                </button>
-                                            </div>
-                                        </div>
-                                        <div className="reviewer">
-                                            <h3 className="fw-bold">
-                                                Alex M.
-                                                <svg
-                                                    xmlns="http://www.w3.org/2000/svg"
-                                                    width="24"
-                                                    height="25"
-                                                    viewBox="0 0 24 25"
-                                                    fill="none"
-                                                >
-                                                    <path
-                                                        d="M12 2.8291C10.0716 2.8291 8.18657 3.40093 6.58319 4.47227C4.97982 5.54362 3.73013 7.06636 2.99218 8.84794C2.25422 10.6295 2.06114 12.5899 2.43735 14.4812C2.81355 16.3725 3.74215 18.1098 5.10571 19.4734C6.46928 20.837 8.20656 21.7656 10.0979 22.1418C11.9892 22.518 13.9496 22.3249 15.7312 21.5869C17.5127 20.849 19.0355 19.5993 20.1068 17.9959C21.1782 16.3925 21.75 14.5075 21.75 12.5791C21.7473 9.99408 20.7192 7.51571 18.8913 5.68783C17.0634 3.85994 14.585 2.83183 12 2.8291ZM16.2806 10.8597L11.0306 16.1097C10.961 16.1795 10.8783 16.2348 10.7872 16.2725C10.6962 16.3103 10.5986 16.3297 10.5 16.3297C10.4014 16.3297 10.3038 16.3103 10.2128 16.2725C10.1218 16.2348 10.039 16.1795 9.96938 16.1097L7.71938 13.8597C7.57865 13.719 7.49959 13.5281 7.49959 13.3291C7.49959 13.1301 7.57865 12.9392 7.71938 12.7985C7.86011 12.6577 8.05098 12.5787 8.25 12.5787C8.44903 12.5787 8.6399 12.6577 8.78063 12.7985L10.5 14.5188L15.2194 9.79848C15.2891 9.72879 15.3718 9.67352 15.4628 9.63581C15.5539 9.59809 15.6515 9.57868 15.75 9.57868C15.8486 9.57868 15.9461 9.59809 16.0372 9.63581C16.1282 9.67352 16.2109 9.72879 16.2806 9.79848C16.3503 9.86816 16.4056 9.95088 16.4433 10.0419C16.481 10.133 16.5004 10.2306 16.5004 10.3291C16.5004 10.4276 16.481 10.5252 16.4433 10.6163C16.4056 10.7073 16.3503 10.79 16.2806 10.8597Z"
-                                                        fill="#01AB31"
-                                                    />
-                                                </svg>
-                                            </h3>
-                                        </div>
-                                        <div className="review-comm fs-6">
-                                            <p>
-                                                &quot;The t-shirt exceeded my
-                                                expectations! The colors are
-                                                vibrant and the print quality is
-                                                top-notch. Being a UI/UX
-                                                designer myself, i&apos;m quite
-                                                picky about aesthetics, and this
-                                                t-shirt definitely gets a thumbs
-                                                up from me.&quot;
-                                            </p>
-                                        </div>
-                                        <div className="review-date fs-6 text-secondary">
-                                            <p className="m-0">
-                                                Posted on August 15, 2023
-                                            </p>
-                                        </div>
-                                    </div>
+                                    < ProductReview />
+                                    < ProductReview />
+                                    < ProductReview />
+                                    < ProductReview />
+                                    < ProductReview />
+                                    < ProductReview />
                                 </div>
                             </div>
                             <div id="tab-pane-3" className="d-none">
